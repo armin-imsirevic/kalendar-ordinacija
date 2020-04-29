23d08cf0b10edd487824cb020cd41b604f2570a9
Armin Imsirevic <armin.imsirevic@oradian.com>
2020-04-29 23:59:33 +0200

    Render the schedule dinamicaly

--- src/component/Component.tsx
+++ src/component/Component.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { connect } from 'react-redux';
+
 import { IState } from '../state/reducer';
 import { saveAppointmentAction, removeAppointmentAction } from '../state/actions';
 import { TimeSelect } from './TimeSelect';
 import { IAppointment } from '../state/interface';
+import { DAYS, APPOINTMENT_TIMES } from '../constants';
 
 export interface IComponentProps {
     appointments: IAppointment[],
@@ -24,22 +26,68 @@ const mapDispatchToProps = dispatch => {
     }
 };
 
+function getMonday(d) {
+    d = new Date(d);
+    var day = d.getDay(), diff = d.getDate() - day + (day === 0 ? -6:1); // adjust when day is sunday
+    return new Date(d.setDate(diff));
+}
+
 class Component extends React.Component<IComponentProps, {error: any, appointment: IAppointment}> {
     date = new Date();
 
     render () {
         const isEven = this.date.getDate() % 2 !== 0;
+        // const currentDate = new Date();
+        const daysArray = Object.values(DAYS);
         return (
-            <form onSubmit={this.handleSubmit}>
-                <input type='number' name='id' onChange={this.handleInputChange} required/>
-                <input type='text' name='name' onChange={this.handleInputChange} required/>
-                <input type='date' name='date' onChange={this.handleInputChange} required/>
-                <TimeSelect isEven={isEven} onChange={this.handleInputChange} required/>
+            <div className='container'>
+                <form onSubmit={this.handleSubmit}>
+                    <input type='number' name='id' onChange={this.handleInputChange} required/>
+                    <input type='text' name='name' onChange={this.handleInputChange} required/>
+                    <input type='date' name='date' onChange={this.handleInputChange} required/>
+                    <TimeSelect isEven={isEven} onChange={this.handleInputChange} required/>
 
-                <input type='submit'/>
-                <div>{this.state && this.state.error ? this.state.error : ''}</div>
-                <div>{this.props && this.props.appointments ? JSON.stringify(this.props.appointments) : ''}</div>
-            </form>
+                    <input type='submit'/>
+                    <div>{this.state && this.state.error ? this.state.error : ''}</div>
+                    <div>{this.props && this.props.appointments ? JSON.stringify(this.props.appointments) : ''}</div>
+                </form>
+                <div className='appointments'>
+                    {
+                        daysArray.map((d, i) => {
+                            const monday = getMonday(new Date());
+                            const date = new Date(monday.setDate(monday.getDate() + i));
+                            const isEven = date.getDate() % 2 === 0;
+                            const ids = this.props.appointments.filter((a) => a.id !== null ).map((a) => a.id);
+                            return (
+                                <div key={i} className='day'>
+                                    <div>{d} - {date.getDate()}/{date.getMonth()}/{date.getFullYear()}</div>
+                                    {
+                                        APPOINTMENT_TIMES.filter((at) => at.even === isEven).map((at, i) => {
+                                            return (
+                                                <div key={i} className='appointment'>
+                                                    <div className='time'>{at.time}</div>
+                                                    <div
+                                                        className='save-icon'
+                                                        onClick={() =>
+                                                            this.props.saveAppointment({
+                                                                date: date.toDateString(),
+                                                                time: at.time,
+                                                                id: ids.length ? Math.max(...ids) + 1 : 1,
+                                                                name: 'Armin Imsirevic'
+                                                            })
+                                                        }
+                                                    />
+                                                    <input className='input-name' type='text' name='name'/>
+                                                </div>
+                                            )
+                                        })
+                                    }
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+            </div>
         );
     }
 
@@ -63,3 +111,4 @@ class Component extends React.Component<IComponentProps, {error: any, appointmen
 };
 
 export const ConnectedComponent = connect(mapStateToProps, mapDispatchToProps)(Component);
+
---
+++ src/constants.ts
@@ -0,0 +1,127 @@
+interface IAppointmentTimes {
+    even: boolean;
+    time: string;
+    break: boolean;
+}
+
+export const APPOINTMENT_TIMES: IAppointmentTimes[] = [
+    {
+        even: true,
+        time: '08:00',
+        break: false,
+    },
+    {
+        even: true,
+        time: '08:30',
+        break: false,
+    },
+    {
+        even: true,
+        time: '09:00',
+        break: false,
+    },
+    {
+        even: true,
+        time: '09:30',
+        break: false,
+    },
+    {
+        even: true,
+        time: '10:00',
+        break: false,
+    },
+    {
+        even: true,
+        time: '10:30',
+        break: false,
+    },
+    {
+        even: true,
+        time: '11:00',
+        break: false,
+    },
+    {
+        even: true,
+        time: '11:30',
+        break: false,
+    },
+    {
+        even: true,
+        time: '12:00',
+        break: false,
+    },
+    {
+        even: true,
+        time: '12:30',
+        break: false,
+    },
+    {
+        even: true,
+        time: '13:00',
+        break: false,
+    },
+    {
+        even: true,
+        time: '13:30',
+        break: false,
+    },
+    {
+        even: false,
+        time: '14:00',
+        break: false,
+    },
+    {
+        even: false,
+        time: '14:30',
+        break: false,
+    },
+    {
+        even: false,
+        time: '15:00',
+        break: false,
+    },
+    {
+        even: false,
+        time: '15:30',
+        break: false,
+    },
+    {
+        even: false,
+        time: '16:00',
+        break: false,
+    },
+    {
+        even: false,
+        time: '16:30',
+        break: false,
+    },
+    {
+        even: false,
+        time: '17:00',
+        break: false,
+    },
+    {
+        even: false,
+        time: '17:30',
+        break: false,
+    },
+    {
+        even: false,
+        time: '18:00',
+        break: false,
+    },
+    {
+        even: false,
+        time: '18:30',
+        break: false,
+    },
+]
+
+export const DAYS = {
+    Monday: 'Ponedeljak',
+    Tuesday: 'Utorak',
+    Wednesday: 'Srijeda',
+    Thursday: 'Četvrtak',
+    Friday: 'Petak',
+    Saturday: 'Subota',
+}
